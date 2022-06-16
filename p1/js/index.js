a414f9d163ae0a8e36fc3408991e44d6fb126fb0
zhaodesheng <zhaodesheng@chemical.ai>
2022-06-16 14:50:30 +0800

    生成随机图案

--- p1/js/index.js
+++ p1/js/index.js
@@ -1,63 +1,112 @@
-var color1Dom = getId('color1');
+var color1Dom = getId("color1");
 
-// 生成预览图案
-function generatePreviewPattern (color = "#000") {
-  let domStr = '';
-  patterns.forEach(item => {
-    let tableContent = '';
-    item.data.forEach(row => {
-      let rowContent = '<tr>';
-      row.forEach(column => {
-        rowContent += `<td style="background: ${column === 1 ? color : '' }"></td>`;
-      });
-      tableContent += rowContent + '</tr>';
+/**
+ * 生成图案表格的DOM字符串
+ * @param {Array} data
+ * @param {string} color
+ * @return {string}
+ */
+function getPatternTable(data, color = "#000") {
+  let tableContent = "";
+  data.forEach((row) => {
+    let rowContent = "<tr>";
+    row.forEach((column) => {
+      rowContent += `<td style="background: ${column === 1 ? color : 'transparent'}"></td>`;
     });
+    tableContent += rowContent + "</tr>";
+  });
+  let tableStr = `
+    <table class="pattern-table">
+      <tbody>
+        ${tableContent}
+      </tbody>
+    </table>
+  `;
+  return tableStr;
+}
+
+/**
+ * 生成预览图案
+ * @param {String} color
+ */
+function generatePreviewPattern(color = "#000") {
+  let domStr = "";
+  patterns.forEach((item) => {
+    let tableStr = getPatternTable(item.data, color);
     domStr += `
-      <li class="pattern-item">
-        <p class="name">${item.name}</p>
-        <table class="pattern-preview">
-          <tbody>
-            ${tableContent}
-          </tbody>
-        </table>
-      </li>
+    <li class="pattern-item">
+      <p class="name">${item.name}</p>
+      ${tableStr}
+    </li>
     `;
   });
 
-  getDom('.patterns')[0].innerHTML = domStr;
+  getDom(".patterns")[0].innerHTML = domStr;
 }
 
 generatePreviewPattern();
 
-function clickConfirm() {
-  var color1 = color1Dom.value;
-  var color2 = color2Dom.value;
-  var color3 = color3Dom.value;
-  var color4 = color4Dom.value;
-  var smallSquareWidth = +widthDom.value || 20;
-  var sideSquareCount = +countDom.value || 10;
+/**
+ * 生成空画板
+ * @param {number} row 行数
+ * @param {number} column 列数
+ */
+function generatePanel(row, column) {
+  let domStr = "";
+  for (let i = 0; i < row; i++) {
+    let rowContent = "<tr>";
+    for (let j = 0; j < column; j++) {
+      rowContent += `<td></td>`;
+    }
+    domStr += rowContent + "</tr>";
+  }
 
-  var width = smallSquareWidth * 2 * sideSquareCount
-  canvas.width = width;
-  canvas.height = width;
+  getDom(".panel-table tbody")[0].innerHTML = domStr;
+}
 
-  var ctx = canvas.getContext('2d');
-  console.log(ctx)
-  for (var row = 0; row < sideSquareCount; row ++) {
-    for (var column = 0; column < sideSquareCount; column ++) {
-      var x0 = column * smallSquareWidth * 2;
-      var y0 = row * smallSquareWidth * 2;
-      var colors = [color1, color2, color3, color4];
-      for (var index = 0; index < 4; index ++) {
-        var x1 = x0 + (index % 2) * smallSquareWidth;
-        var y1 = y0 + ~~(index / 2) * smallSquareWidth;
-        var randomIndex = (Math.random() * colors.length) >> 0;
-        // var color = colors.splice(randomIndex, 1)[0]; // 局部小正方形随机
-        var color = colors[randomIndex]; // 每一个位置随机颜色
-        // console.log(`%c${color}`, `color: ${color}`)；
-        ctx.fillStyle = color;
-        ctx.fillRect(x1, y1, smallSquareWidth, smallSquareWidth);
-      }
+/**
+ * 行数
+ */
+const rowCount = 2;
+/**
+ * 列数
+ */
+const columnCount = 5;
+generatePanel(rowCount, columnCount);
+
+/**
+ * 填充单个图案
+ * @param {{data: Array; color: string;}} pattern
+ * @param {{rowIndex: number; columnIndex: number;}} position
+ */
+function fillPatternToPanel(pattern, position) {
+  const { rowIndex, columnIndex } = position;
+  const td = getDom(`.panel-table > tbody > tr:nth-of-type(${rowIndex + 1}) > td:nth-of-type(${columnIndex + 1})`)[0];
+  td.innerHTML = getPatternTable(pattern.data, pattern.color);
+}
+
+/**
+ * 用随机图案和随机颜色填充画布
+ */
+function randomFillPanel() {
+  for (let i = 0; i < rowCount; i++) {
+    for (let j = 0; j < columnCount; j++) {
+      console.log(
+        {
+          data: patterns[~~(Math.random() * patterns.length)].data,
+          color: "#" + Math.random().toString(16).slice(-6),
+        },
+        { rowIndex: i, columnIndex: j }
+      );
+      fillPatternToPanel(
+        {
+          data: patterns[~~(Math.random() * patterns.length)].data,
+          color: "#" + Math.random().toString(16).slice(-6),
+        },
+        { rowIndex: i, columnIndex: j }
+      );
     }
   }
 }
+
+randomFillPanel();
