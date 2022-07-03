285a2423f8836e2272be2767f387852ba4f3c4d9
zhaodsh <zhaodesheng@chemical.ai>
2022-07-03 19:40:33 +0800

    增加颜色配置

--- p1/js/index.js
+++ p1/js/index.js
@@ -31,13 +31,12 @@ function getPatternTable(data, options = {}) {
     tableContent += rowContent + "</tr>";
   });
   let tableStr = `
-    <table class="pattern-table">
+    <table class="pattern-table" data-data='${JSON.stringify({ data, color })}'>
       <tbody>
         ${tableContent}
       </tbody>
-    </table>
+    </table>  
   `;
-  console.log(tableStr);
   return tableStr;
 }
 
@@ -46,10 +45,10 @@ function getPatternTable(data, options = {}) {
  * @param {String} color
  */
 function generatePreviewPattern(color = "#000") {
-  
+
   let domStr = "";
   patterns.forEach((item) => {
-    let tableStr = getPatternTable(item.data, {color});
+    let tableStr = getPatternTable(item.data, { color });
     domStr += `
     <li class="pattern-item">
       <p class="name">${item.name}——${item.count}</p>
@@ -91,7 +90,7 @@ function confirmPick() {
       pickedPatterns.push(patterns[index]);
     }
   });
-  console.log(pickedPatterns, 'pickedPatterns');
+  // console.log(pickedPatterns, 'pickedPatterns');
 
   renderPickedPatterns();
 }
@@ -120,7 +119,7 @@ function renderPickedPatterns() {
     domStr += `
       <li class="picked-pattern-item">
         <p class="name">${item.name}——${item.count}</p>
-        ${getPatternTable(item.data, {color: "#000"})}
+        ${getPatternTable(item.data, { color: "#000" })}
         <ol class="colors">
           <li class="color-item">
             <input type="color" value="#000000" />
@@ -143,7 +142,6 @@ function renderPickedPatterns() {
 function bindDeleteColor() {
   [...getDom('.delete-color')].forEach((item) => {
     item.onclick = () => {
-      console.log('delete')
       item.parentElement.remove();
     }
   });
@@ -202,7 +200,7 @@ function generatePanel(row, column) {
 function fillPatternToPanel(pattern, position, options) {
   const { rowIndex, columnIndex } = position;
   const td = getDom(`.panel-table > tbody > tr:nth-of-type(${rowIndex + 1}) > td:nth-of-type(${columnIndex + 1})`)[0];
-  td.innerHTML = getPatternTable(pattern.data, {color: pattern.color, size: options.size});
+  td.innerHTML = getPatternTable(pattern.data, { color: pattern.color, size: options.size });
 }
 
 /**
@@ -285,11 +283,11 @@ function downloadImg() {
   //     panelDom.classList.add('preview');
   //   });
   // }, 30);
-  
+
   const rowCount = +getDom('.width-count')[0].value || 2;
   const columnCount = +getDom('.height-count')[0].value || 5;
   const squareWidth = +getDom('.square-width')[0].value || 10;
-  
+
   const scale = devicePixelRatio;
   var copyDom = panelDom.cloneNode(true);
   const width = columnCount * 11 * squareWidth;
@@ -380,7 +378,7 @@ const heightCount = 13; // 每个图案高的小正方向的数量
  * @param {MouseEvent} e 
  */
 panel.onmousedown = function (e) {
-  console.log(e);
+  // console.log(e);
   const { pageX, pageY } = e;
   const panelLeft = panel.offsetLeft;
   const panelTop = getPageTop(panel);
@@ -460,13 +458,6 @@ function exchange(dom1, dom2) {
 function randomFillPanel() {
   for (let i = 0; i < rowCount; i++) {
     for (let j = 0; j < columnCount; j++) {
-      // console.log(
-      //   {
-      //     data: patterns[~~(Math.random() * patterns.length)].data,
-      //     color: "#" + Math.random().toString(16).slice(-6),
-      //   },
-      //   { rowIndex: i, columnIndex: j }
-      // );
       fillPatternToPanel(
         {
           data: patterns[~~(Math.random() * patterns.length)].data,
@@ -479,3 +470,172 @@ function randomFillPanel() {
 }
 
 // randomFillPanel();
+
+let colorList = [];
+// let colorList = [
+//   {
+//     color: '#ff0000',
+//     rate: 0.3,
+//   }, {
+//     color: '#000000',
+//     rate: 0.25,
+//   }, {
+//     color: '#cccccc',
+//     rate: 0.3,
+//   }, {
+//     color: '#ffffff',
+//     rate: 0.15,
+//   }
+// ].map(item => {
+//   item.color = colorTransform(item.color);
+//   return item;
+// });
+
+// 生成颜色旁边放个的颜色
+function getSideColor(excludeColor) {
+  const colors = colorList.filter(item => item.color !== excludeColor);
+  return colors[Math.floor(Math.random() * colors.length)].color;
+}
+
+function fillWithRandomColor() {
+  // 要求：图案周围的颜色不能与图案颜色相同
+  // 1. 获取图案外测一圈的坐标，填充与图案不一样的随机颜色
+  // 2. 用随机色填充剩余位置
+
+  // 获取配置的颜色
+  [...getDom('.fill-color-item')].forEach((item) => {
+    const color = colorTransform(item.getElementsByClassName('color')[0].value);
+    const rate = item.getElementsByClassName('rate')[0].value / 100;
+    colorList.push({ color, rate });
+  });
+
+  const panelPatters = getDom('.panel-table .pattern-table');
+  // console.log(panelPatters);
+
+  const rowCount = +getDom('.width-count')[0].value || 2;
+  const columnCount = +getDom('.height-count')[0].value || 5;
+  // 1. 将多个表格生成一个二维的颜色数组
+  const arr = [];
+  for (let i = 0; i < rowCount * 13; i++) {
+    arr.push([]);
+  }
+  for (let i = 0, len = panelPatters.length; i < len; i++) {
+    const originRowIndex = Math.floor(i / columnCount);
+    const originColumnIndex = i % columnCount;
+
+    const item = panelPatters[i];
+    item.setAttribute('data-index', i);
+    const { data, color } = JSON.parse(item.getAttribute('data-data'));
+    // console.log(item);
+    data.forEach((row, rowIndex) => {
+      row.forEach((column, columnIndex) => {
+        arr[originRowIndex * 13 + rowIndex][originColumnIndex * 11 + columnIndex] = column ? color : null;
+      });
+    });
+  }
+
+  // console.log(arr);
+
+  // 2. 将图案周围的一圈填充其他颜色
+  const copyArr = JSON.parse(JSON.stringify(arr));
+  copyArr.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
+      // 判断上下左右是否有颜色 （数组取不到的坐标返回undefined）
+      // 因为数组是从左向右，从上向下访问，所以不需要判断上和左
+      if (column) {
+        return;
+      }
+      // 上有颜色
+      if (arr[rowIndex - 1] && arr[rowIndex - 1][columnIndex]) {
+        row[columnIndex] = getSideColor(arr[rowIndex - 1][columnIndex]);
+      }
+      // 下有颜色
+      if (arr[rowIndex + 1] && arr[rowIndex + 1][columnIndex]) {
+        row[columnIndex] = getSideColor(arr[rowIndex + 1][columnIndex]);
+      }
+      // 左有颜色
+      if (arr[rowIndex] && arr[rowIndex][columnIndex - 1]) {
+        row[columnIndex] = getSideColor(arr[rowIndex][columnIndex - 1]);
+      }
+      // 右有颜色
+      if (arr[rowIndex] && arr[rowIndex][columnIndex + 1]) {
+        row[columnIndex] = getSideColor(arr[rowIndex][columnIndex + 1]);
+      }
+    });
+  });
+
+  // 3. 根据颜色比例填充剩余的方格
+  const totalCount = rowCount * columnCount * 11 * 13;
+  let hasColorCount = 0;
+  const statisticObj = {}; // 存放已有的颜色统计
+  copyArr.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
+      if (column) {
+        hasColorCount++;
+        statisticObj[column] = statisticObj[column] ? statisticObj[column]++ : 1;
+      }
+    });
+  });
+  const restNullCount = totalCount - hasColorCount;
+  const calculateColorCount = colorList.map(item => {
+    return {
+      color: item.color,
+      count: Math.ceil(item.rate * restNullCount), // 先线上取整，多余的后面截取
+    }
+  });
+
+  const toFillColors = []; // 待填充的所有颜色
+  calculateColorCount.forEach(item => {
+    for (let i = 0; i < item.count; i++) {
+      toFillColors.push(item.color);
+    }
+    // array.fill(value, start, end)
+    // toFillColors.fill(item.color, toFillColors.length, toFillColors.length + item.count);
+  });
+  toFillColors.length = restNullCount;
+
+  copyArr.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
+      if (column) {
+        return;
+      }
+      const randomIndex = Math.floor(Math.random() * toFillColors.length);
+      row[columnIndex] = toFillColors.splice(randomIndex, 1)[0];
+    });
+  });
+
+  // console.log(copyArr);
+
+  // 4. 将数组中的色值对应到界面上
+  copyArr.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
+      const tableIndex = Math.floor(rowIndex / 13) * columnCount + Math.floor(columnIndex / 11);
+      const trIndex = rowIndex % 13;
+      const tdIndex = columnIndex % 11;
+      panelPatters[tableIndex].getElementsByTagName('tr')[trIndex].getElementsByTagName('td')[tdIndex].style.background = column;
+    });
+  });
+}
+
+getDom('.fillWithRandomColor')[0].onclick = fillWithRandomColor;
+
+// 绑定删除颜色配置项
+function bindDeleteFillColor() {
+  [...getDom('.delete-fill-color')].forEach(item => {
+    item.onclick = () => item.parentNode.remove();
+  });
+}
+
+bindDeleteFillColor();
+
+getDom('.add-fill-color')[0].onclick = function () {
+  getDom('.fill-color-list')[0].innerHTML += `
+    <li class="fill-color-item">
+      颜色：<input type="color" class="color" /><br />
+      比例：<input type="number" class="rate" placeholder="输入0-100" />%<br />
+      <button class="delete-fill-color">删除</button>
+    </li>
+  `;
+
+  bindDeleteFillColor();
+}
